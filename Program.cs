52c29cf5390b05c09654b0a5fb591def2d4659aa
Morpiha <Morpiha@live.se>
2014-09-08 18:26:09 +0200

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0.0;
+            double GivenSum = 0.0;
+
+            Console.WriteLine("Ange totalsumma: ");
+            sum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Erhållet belop: ");
+            GivenSum = int.Parse(Console.ReadLine());
+        }
+    }
+}
